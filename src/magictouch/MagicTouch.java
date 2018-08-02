279b3ccdd8dba0ee90935a467b15ec3e5589caad
LorenzoPels <lorenzo-pelliccia@hotmail.it>
2018-08-02 11:27:31 +0200

    secondo commit

--- src/magictouch/MagicTouch.java
+++ src/magictouch/MagicTouch.java
@@ -15,7 +15,7 @@ public class MagicTouch {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       System.out.println("dio mio"); // TODO code application logic here
+       // TODO code application logic here
     }
     
 }
