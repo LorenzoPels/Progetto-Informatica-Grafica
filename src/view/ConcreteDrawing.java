27bacf90b19594edb57d8de8bd1ab5240269f51a
LorenzoPels <lorenzo-pelliccia@hotmail.it>
2018-08-14 17:40:43 +0200

    inizio di model con initgame, da vedere l'etichetta del punteggio,settare pausa con p

---
+++ src/view/ConcreteDrawing.java
@@ -0,0 +1,32 @@
+
+package view;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.geom.Rectangle2D;
+import java.awt.image.BufferedImage;
+import java.awt.image.ImageObserver;
+import view.CaricatoreImmagine;
+
+public class ConcreteDrawing extends AbstractDrawing {
+
+    BufferedImage sfondo = null;
+    private void Caricarisorse(){
+    CaricatoreImmagine loader = new CaricatoreImmagine();
+    sfondo = loader.caricaImmagine("/immagine/Sfondo.png");
+    }
+
+    public ConcreteDrawing() {
+        this.sfondo = new BufferedImage(600, 800, 0);  // x, y, width, height
+    }
+
+    @Override
+    public void draw(Graphics g) {
+        Graphics g2d = (Graphics2D)g;
+
+        
+        g2d.drawImage(sfondo,0,0,null, (ImageObserver) this);
+        g2d.dispose();
+    }
+
+} // end class
