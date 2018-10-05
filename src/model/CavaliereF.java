eab239150a1baf2dd29da0791c5f43c4a66102f6
Figgutz <Figgutz@192.168.1.2>
2018-10-05 09:54:10 +0200

    immagini nuove e merge again

--- src/model/CavaliereF.java
+++ src/model/CavaliereF.java
@@ -5,6 +5,6 @@ package model;
 public class CavaliereF extends Cavaliere {
     CavaliereF(){
     this.nomeCavaliere = "F";
-    this.coloreCavaliere = "Giallo";
+    this.coloreCavaliere = "Rosso";
     }
 }
