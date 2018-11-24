680cfb5eb7f34ce8c965ac6884fc65e4d8aabc10
LorenzoPels <lorenzo-pelliccia@hotmail.it>
2018-11-24 19:16:34 +0100

    Model: gestire e contenere tutte variabili di istanza Controller: contenere logica del gioco, richiama direttamente Model per settare i dati, chiama controller for model per leggerte i dati

--- src/controller/ControllerForView.java
+++ src/controller/ControllerForView.java
@@ -2,7 +2,19 @@
 package controller;
 
 
+import static java.lang.Thread.sleep;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import model.Cavaliere;
+import model.MagoDefault;
+import model.Model;
+import view.MainGUI;
 import static view.MainGUI.ALTEZZA;
+import static view.RightPanel.updateScoreLabel;
+import static view.MainGUI.gameover;
+import static view.MainGUI.player;
+import static view.MainGUI.scoppio;
+import static view.MainGUI.timer;
 import view.View;
 
 
@@ -45,47 +57,60 @@ public class ControllerForView implements IControllerForView {
             View.getInstance().closeGameOverDialog();
     }
     public void initGame() {
-            ControllerForModel.getInstance().initGame();
+            Model.getInstance().initGame();
     }
     public String getScore() {
             return String.valueOf(ControllerForModel.getInstance().getScore());
     }
 
     public void incrementScore() {
-            ControllerForModel.getInstance().incrementScore();
+            Model.getInstance().incrementScore();
     }
                                                                                 
 
-    public int getY(int i) {
+   /* public int getY(int i) {
         return ControllerForModel.getInstance().getY(i);
-    }
+    }*/
 
-    public void resetIndex(){
+    /*public void resetIndex(){
         ControllerForModel.getInstance().resetIndex();
                                                                                 //    for (int i=0; i<index.length;i++)
                                                                                 //       index[i]=0;
-    }
+    }*/
 
-    public void resetY(){
+   /* public void resetY(){
         ControllerForModel.getInstance().resetY();
                                                                                 //    for (int i=0; i<y.length;i++)
                                                                                 //        y[i]=-150;  
-    }
+    }*/
+    
     
     public void statoCavaliere(int i){
-        if(controlloreMovimento[i]) 
-            y[i]+= ControllerForModel.getInstance().getMovimento();
-            if(esplosi[i])
-                y[i]+=movimento+1;
-            if(index[i] > 12)
-                y[i]+=2*movimento+1;
-        if(y[i] >= -1000) controlloreMovimento[i] = true;
-        if(y[i] >= altezzaterreno) controlloreMovimento[i] = false;        
-        if((y[i] >= altezzaterreno) &&(esplosi[i] == true)){
-            pioggia[i]= null;
-            index[i]=0;
+        // non si sa colore[i] da dove nasceva fuori, perciò lo richiamo qui come
+        // unica variabile.
+        String colore=null;
+        int y=ControllerForModel.getInstance().getY(i);
+        Boolean esplosi = ControllerForModel.getInstance().getEsplosi(i);
+        int altezzaterreno = ControllerForModel.getInstance().getAltezzaterreno();
+        int movimento = ControllerForModel.getInstance().getMovimento();
+        int index = ControllerForModel.getInstance().getIndex(i);
+        if(ControllerForModel.getInstance().getControlloreMovimento(i)) 
+            
+            y+= ControllerForModel.getInstance().getMovimento();
+            if(esplosi)
+                y+=movimento+1;
+                Model.getInstance().setY(i, y);
+            if(index > 12)
+                y+=2*movimento+1;
+                Model.getInstance().setY(i, y);
+        if(y >= -1000) Model.getInstance().setControlloreMovimento(i, true);
+        if(y >= altezzaterreno) Model.getInstance().setControlloreMovimento(i, false);        
+        if((y >= altezzaterreno) &&(esplosi == true)){
+            Model.getInstance().setPioggia(i, null);
+            index=0;
+            Model.getInstance().setIndex(i, index);
         }        
-        if((y[i] >= altezzaterreno) &&(esplosi[i] == false) ){
+        if((y >= altezzaterreno) &&(esplosi == false) ){
             gameover.play();
             timer.stop();
             try {
@@ -96,19 +121,20 @@ public class ControllerForView implements IControllerForView {
             }
 
             player.stop();
-            ControllerForView.getInstance().openGameOverDialog(scorelabel.getText());
+            ControllerForView.getInstance().openGameOverDialog(/*scorelabel.getText()*/getScore());
 
         }
-        if(esplosi[i] == false){
-             colore[i] = cavalieri[i].getColore();
+        if(esplosi == false){
+             colore = ControllerForModel.getInstance().getCavalieri(i).getColore();
         }
 
-        if(esplosi[i]==true  && index[i] <= 13 && y[i] <= altezzaterreno-1){
-            pioggia[i]= effettuaAnimazione(colore[i], index[i]);
-            index[i] ++;
+        if(esplosi==true  && index <= 13 && y <= altezzaterreno-1){
+             Model.getInstance().setPioggia(i,Model.getInstance().effettuaAnimazione(colore, index));
+            index ++;
+            Model.getInstance().setIndex(i, index);
         }
     }
-        public void pioggiaRandom(){   
+       /* public void pioggiaRandom(){   
             for(int i=0;i<pioggia.length;i++){
                 x[i] = (int)(Math.random() * (larghezza-295)) % (larghezza-295);
                 if(x[i]<148)
@@ -120,26 +146,35 @@ public class ControllerForView implements IControllerForView {
                         x[i] = x[i-1]+148;
                 } 
             }               
-        }
+        }*/
         public void Colpito(boolean b, String s){
+            Cavaliere[] cavalieri = ControllerForModel.getInstance().getCavalieri();
+            int[] y = ControllerForModel.getInstance().getYArray();
+            Boolean[] esplosi = ControllerForModel.getInstance().getEsplosi();
             for(int i =0;(i<cavalieri.length)&&(b==false);i++){
                 if((cavalieri[i].getName()== s) && (esplosi[i]==false)&&(y[i]>-150)){
-                    pioggia[i] = null;
+                    //pioggia[i] = null;
+                    Model.getInstance().setPioggia(i, null);
                     cavalieri[i] = null;
+                    Model.getInstance().setCavalieri(i, cavalieri[i]);
                     esplosi[i] = true;
+                    Model.getInstance().setEsplosi(i, esplosi[i]);
                     cavalieri[i]=Cavaliere.nextCavaliere();
+                    Model.getInstance().setCavalieri(i, cavalieri[i]);
                     scoppio.play();                
-                    mago.gestisciMago(i);
+                    MagoDefault.getInstance().gestisciMago(i);
                     incrementScore();
-                    updateScoreLabel(getScore());
+                    updateScoreLabel(ControllerForModel.getInstance().getScore());
                     b = true;
                 }
             }
         }
         
-        public int getMovimento(){
+        
+        
+       /* public int getMovimento(){
             return movimento;    
-        }
+        }*/
         
         
         
